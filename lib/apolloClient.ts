15fbb802c7b12b90904faef3295cfeededf94ac4
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-07-06 08:24:55 +0900

    initializeApollo関数を定義。サーバ側とクライアント側で挙動が変えられるすごい関数式！

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -21,3 +21,15 @@ const createApolloClient = () => {
     cache: new InMemoryCache(),
   })
 }
+
+const initializeApollo = (initialState = null) => {
+  //左辺の式がapolloClientがnull,undefinedなら右辺が実行される式。
+  const _apolloClient = apolloClient ?? createApolloClient()
+  // For SSG and SSR always create a new Apollo Client
+  //サーバ側ではapolloClientに代入する式がないので毎回createApolloClientが実行される
+  if (typeof window === 'undefined') return _apolloClient
+  // Create the Apollo Client once in the client
+  if (!apolloClient) apolloClient = _apolloClient
+
+  return _apolloClient
+}

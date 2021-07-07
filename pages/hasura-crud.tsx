1fe3e77e71de0c1774522fd1c9e823dd942903f8
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-07-07 22:02:23 +0900

    inputFormとsubmit関数を実装。動作確認おk！😃

--- pages/hasura-crud.tsx
+++ pages/hasura-crud.tsx
@@ -15,6 +15,7 @@ import {
 import { Layout } from '../components/Layout'
 
 const HasuraCRUD: VFC = () => {
+  const [editedUser, setEditedUser] = useState({ id: '', name: '' })
   const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
     fetchPolicy: 'cache-and-network',
   })
@@ -46,9 +47,65 @@ const HasuraCRUD: VFC = () => {
     },
   })
 
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    //editeduserのidが既存であれば更新、なければ新規作成。
+    if (editedUser.id) {
+      try {
+        await update_users_by_pk({
+          variables: {
+            id: editedUser.id,
+            name: editedUser.name,
+          },
+        })
+      } catch (error) {
+        alert(error.message)
+      }
+      setEditedUser({ id: '', name: '' })
+    } else {
+      try {
+        await insert_users_one({
+          variables: {
+            name: editedUser.name,
+          },
+        })
+      } catch (error) {
+        error.message
+      }
+      setEditedUser({ id: '', name: '' })
+    }
+  }
+
+  //クエリでエラーになった際のレンダリングの記述
+  if (error) return <Layout title="Hasura CRUD">Error: {error.message}</Layout>
+
   return (
     <Layout title="Hasura CRUD">
       <p className="mb-3 font-bold">Hasura CRUD</p>
+      <form
+        className="flex flex-col justify-center items-center"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="New User ?"
+          className="px-3 py-2 border border-gray-300"
+          value={editedUser.name}
+          onChange={(e) => {
+            setEditedUser({ ...editedUser, name: e.target.value })
+          }}
+        />
+        <button
+          type="submit"
+          disabled={!editedUser.name}
+          className="disabled:opacity-40 my-3 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none"
+          data-testid="new"
+        >
+          {editedUser.id ? 'Update' : 'Create'}
+        </button>
+      </form>
     </Layout>
   )
 }
+
+export default HasuraCRUD

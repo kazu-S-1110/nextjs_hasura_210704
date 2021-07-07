f05d838d37809ad932a5c1b3fac528bb33aa7762
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-07-07 21:41:55 +0900

    ユーザの新規作成する関数を実装。キャッシュの扱いが大変や、、

---
+++ pages/hasura-crud.tsx
@@ -0,0 +1,41 @@
+import { VFC, useState, FormEvent } from 'react'
+import { useQuery, useMutation } from '@apollo/client'
+import {
+  GET_USERS,
+  CREATE_USER,
+  DELETE_USER,
+  UPDATE_USER,
+} from '../queries/queries'
+import {
+  GetUsersQuery,
+  CreateUserMutation,
+  DeleteUserMutation,
+  UpdateUserMutation,
+} from '../types/generated/graphql'
+import { Layout } from '../components/Layout'
+
+const HasuraCRUD: VFC = () => {
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
+    fetchPolicy: 'cache-and-network',
+  })
+  const [update_users_by_pk] = useMutation<UpdateUserMutation>(UPDATE_USER)
+  //ユーザの新規作成・削除の場合、キャッシュは自動で反映されないのでキャッシュを読むよう記述する必要がある。
+  const [insert_users_one] = useMutation<CreateUserMutation>(CREATE_USER, {
+    update(cache, { data: { insert_users_one } }) {
+      const cacheId = cache.identify(insert_users_one)
+      cache.modify({
+        fields: {
+          users(existingUsers, { toReference }) {
+            return [toReference(cacheId), ...existingUsers]
+          },
+        },
+      })
+    },
+  })
+
+  return (
+    <Layout title="Hasura CRUD">
+      <p className="mb-3 font-bold">Hasura CRUD</p>
+    </Layout>
+  )
+}
